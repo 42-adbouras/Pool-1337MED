7394081971b60bbf88b539a2f0080d48881f2b48
Adham Bouras <wikaz077@gmail.com>
2024-01-18 21:06:00 +0100

    85%

---
+++ C00/ex07/ft_putnbr.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adbouras <adbouras@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/26 14:46:34 by adbouras          #+#    #+#             */
+/*   Updated: 2023/10/28 15:50:46 by adbouras         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print(char num)
+{
+	write(1, &num, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb == -2147483648)
+	{
+		write(1, "-2147483648", 11);
+	}
+	else if (nb >= 0 && nb <= 9)
+	{
+		nb = nb + '0';
+		ft_print(nb);
+	}
+	else if (nb < 0)
+	{
+		ft_print('-');
+		ft_putnbr(nb * -1);
+	}
+	else if (nb > 9)
+	{
+		ft_putnbr(nb / 10);
+		ft_putnbr(nb % 10);
+	}
+}
